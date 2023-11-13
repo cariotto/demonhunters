5df5a160fa9b684522a2d41044eede738a0a0eed
inamorta <inamortaoliver@gmail.com>
2023-11-13 11:58:10 -0300

    alteração

---
+++ site/inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Principal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body class="bg-black">
+    <div class= "context text-center m-10">
+    <body class="image"><img src="dragao9.gif" height="500">
+    <h2 class="text-danger">𝕾𝖆𝖚𝖉𝖆çõ𝖊𝖘 𝖘𝖔𝖇𝖗𝖊𝖛𝖎𝖛𝖊𝖓𝖙𝖊</h2><br>
+        <p class ="text-danger">𝖁𝖔𝖈𝖊 𝖊𝖓𝖙𝖗𝖔𝖚 𝖓𝖆 𝖆𝖛𝖊𝖓𝖙𝖚𝖗𝖆 𝖒𝖆𝖎𝖘 𝖊𝖒𝖔𝖈𝖎𝖔𝖓𝖆𝖓𝖙𝖊 𝖉𝖊 𝖙𝖔𝖉𝖆𝖘 𝖆𝖖𝖚𝖎 𝖔 𝖓𝖔𝖘𝖘𝖔 𝖕𝖊𝖙 𝖌𝖚𝖎𝖆 𝖘𝖕𝖎𝖐𝖊 𝖔 𝖉𝖗𝖆𝖌ã𝖔 𝖊𝖘𝖖𝖚𝖊𝖑𝖊𝖙𝖔 𝖊𝖑𝖊 𝖛𝖆𝖎 𝖙𝖊 𝖌𝖚𝖎𝖆𝖗 𝖊 𝖒𝖔𝖘𝖙𝖗𝖆𝖗, 𝖈𝖔𝖒𝖔 𝖔 𝖏𝖔𝖌𝖔 𝖊𝖘𝖙𝖆 𝖊 𝖖𝖚𝖆𝖓𝖉𝖔 𝖊𝖑𝖊 𝖛𝖆𝖎 𝖑𝖆𝖓ç𝖆𝖗 𝖊𝖑𝖊 𝖙𝖆𝖒𝖇𝖊𝖒 𝖛𝖆𝖎 𝖋𝖆𝖑𝖆𝖗 𝖉𝖊 𝖈𝖔𝖎𝖘𝖆𝖘 𝖎𝖓𝖔𝖛𝖆𝖉𝖔𝖗𝖆𝖘 𝖖𝖚𝖊 𝖓𝖊𝖓𝖍𝖚𝖒 𝖏𝖔𝖌𝖔 𝖚𝖘𝖔𝖚
+        <p class ="text-danger"> 𝖉𝖎𝖌𝖆 𝖔𝖑𝖆 𝖕𝖆𝖗𝖆 𝖔 𝖘𝖕𝖎𝕶𝖊
+       
